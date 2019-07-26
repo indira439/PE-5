4f20c443ae75e6208ff64609a80de66788a5a826
indu <indira.indukuri@cgi.com>
2019-07-26 10:28:37 +0530

    Added comments to UpdateArray class

--- src/main/java/com/stackroute/pe5/UpdateArray.java
+++ src/main/java/com/stackroute/pe5/UpdateArray.java
@@ -1,16 +1,14 @@
 package com.stackroute.pe5;
-
-import java.security.InvalidParameterException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 /**
  * Write a Java program to update specific array element by given element and empty the array list.
  * Input: [Apple, Grape, Melon, Berry]
  * Output: [Kiwi, Grape, Mango, Berry]
  * Array list after removing all elements []
  */
+
+import java.security.InvalidParameterException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class UpdateArray {
     public ArrayList<String> arrayList;
